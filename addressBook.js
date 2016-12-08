a1447b662b8382d4e6ac9e87200cbf122d962992
8611m <7105m@bk.ru>
2016-12-08 05:59:37 +0300

    Доделал контакты

--- addressBook.js
+++ addressBook.js
@@ -19,12 +19,27 @@ var users = [
   } 
 ];
 
-var user = {};
+function addUser(users) {
+	var user = {};
+	user.firstName = prompt('Введите имя:');
+	user.lastName = prompt('Введите фамилию:');
+	user.birthDay = prompt('Дату рождения:');
+	user.phones = [];
+	while (true) {
+		var phone = prompt('Номер телефона:(для выхода введите пустой номер)');
+		if (!phone) break;
+		user.phones.push(phone);
+	}
+	users.push(user);
+}
 
-user.fitstName = prompt('Введите имя');
-user.lastName = prompt('Введите фамилию');
-user.birthDay = prompt('Введите дату рождения');
-user.phone = prompt('Введите номер телефона');
 
 
-user.push(user);
+function csvFormat(users) {
+  return users.map(function(user) {
+    return Object.keys(user).map(function(key) { 
+      if(typeof user[key] == 'string' || typeof user[key] == 'number') return '"' + user[key] + '"'; 
+      else return user[key].map(function(phone) { return '"' + phone + '"'; }).join(';');
+    }).join(';');
+  }).join('\r\n');
+}
